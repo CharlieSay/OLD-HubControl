37b158e135f8972f1cfcd7e552ee6b267ff567fd
Jack Dallas <jack.dallas14@gmail.com>
2013-11-15 20:12:23 +0000

    Added Rank Command (Untested)

---
+++ src/main/java/uk/co/shadycast/shadycontroller/Commands/Rank.java
@@ -0,0 +1,37 @@
+package uk.co.shadycast.shadycontroller.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import uk.co.shadycast.shadycontroller.Objects.SPlayer;
+import uk.co.shadycast.shadycontroller.ShadyController;
+import uk.co.shadycast.shadycontroller.Storage.DB;
+import uk.co.shadycast.shadycontroller.Utils.Msg;
+
+
+public class Rank implements CommandExecutor{
+
+    public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
+    Player p = (Player) sender;
+    SPlayer sp = ShadyController.getPlayer(p);
+    // /Rank <User> <Rank>
+    if(DB.getRankPower(sp.getRank()) >= 8){
+        if (cmd.getLabel().equalsIgnoreCase("Rank")) {
+            if(args.length == 2 ){
+                Player pr = Bukkit.getPlayer(args[0].toString());
+                SPlayer spr = ShadyController.getPlayer(pr);
+                String r = args[1].toString();
+                spr.setRank(r);
+            }else{
+                Msg.Player("Correct usage = /Rank User Rank", p);
+            }
+        }
+    }else{
+        Msg.Player("You don't have perms to do that!", p);
+    }
+    return false;
+    }
+
+}
