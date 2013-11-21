7d7246cb00152f195acfe151dcbe7964e739ce84
Jack Dallas <jack.dallas14@gmail.com>
2013-11-21 15:07:42 +0000

    Ill Fix that later

--- src/main/java/uk/co/shadycast/shadycontroller/Cmds/Gamemode.java
+++ src/main/java/uk/co/shadycast/shadycontroller/Cmds/Gamemode.java
@@ -12,36 +12,36 @@ import uk.co.shadycast.shadycontroller.Utils.Msg;
 import uk.co.shadycast.shadycontroller.Utils.Utils;
 
 
-public class Gamemode implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
-        Player p = (Player) sender;
-        SPlayer sp = ShadyController.getPlayer(p);
-        if(cmd.getLabel().equalsIgnoreCase("Gamemode") || cmd.getLabel().equalsIgnoreCase("gm")){
-            if (DB.getRankPower(sp.getRank()) >= 8) {
-                if(args.length == 1){
-                    if(Utils.isInt(args[0])){
-                        int gm = Integer.parseInt(args[0]);
-                        if(gm == 0){
-                            p.setGameMode(GameMode.SURVIVAL);
-                        }else if(gm == 1){
-                            p.setGameMode(GameMode.CREATIVE);
-                        }else if(gm == 2){
-                            p.setGameMode(GameMode.ADVENTURE);
-                        }else{
-                           Msg.Player("Correct usage /Gamemode <0|1|2>", p); 
-                        }
-                    }else{
-                  Msg.Player("Correct usage /Gamemode <0|1|2>", p);
-                }
-                }else{
-                  Msg.Player("Correct usage /Gamemode <0|1|2>", p);
-                }
-            } else {
-                Msg.Player("Thats a no no", p);
-            }   
-        }
-    return false;
-    }
-
+public class Gamemode /*implements CommandExecutor {
+ * @Override
+ * public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
+ * Player p = (Player) sender;
+ * SPlayer sp = ShadyController.getPlayer(p);
+ * if(cmd.getLabel().equalsIgnoreCase("Gamemode") || cmd.getLabel().equalsIgnoreCase("gm")){
+ * if (DB.getRankPower(sp.getRank()) >= 8) {
+ * if(args.length == 1){
+ * if(Utils.isInt(args[0])){
+ * int gm = Integer.parseInt(args[0]);
+ * if(gm == 0){
+ * p.setGameMode(GameMode.SURVIVAL);
+ * }else if(gm == 1){
+ * p.setGameMode(GameMode.CREATIVE);
+ * }else if(gm == 2){
+ * p.setGameMode(GameMode.ADVENTURE);
+ * }else{
+ * Msg.Player("Correct usage /Gamemode <0|1|2>", p);
+ * }
+ * }else{
+ * Msg.Player("Correct usage /Gamemode <0|1|2>", p);
+ * }
+ * }else{
+ * Msg.Player("Correct usage /Gamemode <0|1|2>", p);
+ * }
+ * } else {
+ * Msg.Player("Thats a no no", p);
+ * }
+ * }
+ * return false;
+ * }*/
+{
 }
